ad81acb682500cc08939662f9c3eb995b53e6566
Pranav Kansara <pranavkansara@gmail.com>
2022-11-03 14:51:36 -0500

    third commit

--- HabitTrackerMain.py
+++ HabitTrackerMain.py
@@ -7,3 +7,15 @@ st.set_page_config(layout='wide',page_title='Habit & Goal Tracker')#,page_icon='
 page = st.sidebar.radio("Select page",['Setup','Daily entry','Dashboard'],index=1)
 
 st.write("Hello")
+
+if os.path.exists('goaldf.pkl'):
+    goaldf  = pd.read_pickle('goaldf.pkl')
+    goaldf['Category'] = 'Physical'
+    goaldf = goaldf[['Date','Goal','Category','Target Unit','Target','Timeline']]
+    goaldf.to_pickle('goaldf.pkl')
+else:
+    goaldf = pd.DataFrame({'Date':np.datetime64, 'Goal':'str','Category':'str','Target Unit':np.number,'Target':'str','Timeline':np.datetime64},index=[])
+# only specify non-text types below
+goaldisptypes = {'Date':'date', 'Category':['Physical','Mental','Emotional','Spiritual','Financial','Family'],'Target':'number','Timeline':'date'}
+
+st.write(goaldf)
